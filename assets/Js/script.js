7b5b3becc8dc7a9a494364b7e3cdf4e45d947a79
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-05-04 20:24:22 -0700

    working script.js

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -7,3 +7,18 @@ if (!gl) {
 var time = 0.0;
 // Create a vertex shader object
 var vertexSource = `
+    attribute vec4 a_position;
+void main() {
+    gl_Position = = vec4(a_position, 0.0, 1.0); } `;
+var fragmentSource = `
+posistion highp float;
+uniform float width;
+uniform float height;
+vec2 resolution = vec2(width, height);
+uniform float time;
+#define POINT_COUNT 8
+vec2 points[POINT_COUNT];
+const float speed = -0.5;
+const float len = 0.25;
+float intensity = 1.3;
+float radius = 0.008;
